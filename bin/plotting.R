1fc696dde95e41ace465f4c1885b2d9d1bb9dcca
Guy Oldrieve <goldriev@sce-bio-c05389.bio.ed.ac.uk>
2024-06-27 09:14:46 +0100

    conditional

--- bin/plotting.R
+++ bin/plotting.R
@@ -1,4 +1,4 @@
-library (dplyr)
+library(dplyr)
 library(RColorBrewer)
 library(reshape)
 library(ggpubr)
@@ -6,10 +6,18 @@ library(pheatmap)
 library(tidyverse)
 
 #Read in the meta data and modify it for tximport
-stats <- read.csv("~/Google Drive/My Drive/vsg/vsgseq2/data/meta.csv")
-orthogroups <- read.csv("~/pkgs/analyse/orthofinder/protein/all_samples/OrthoFinder/Results_May31/Orthogroups/Orthogroups.csv")
-orthogroups_long <- orthogroups %>% separate_rows(Name, sep = ",")
-my.files <-  list.files(list.dirs(path = "/Users/goldriev/pkgs/analyse/all_data_results", full.names = TRUE, recursive = TRUE), pattern = "quant.sf", full.names = TRUE)
+stats <- read.csv("Google Drive/My Drive/vsg/vsgseq2/data/meta.csv")
+orthogroups <- read.csv("~/pkgs/analyse/all_data_results_no_vsgdb/protein/OrthoFinder/Results_Jun18/Orthogroups/Orthogroups.csv")
+orthogroups_long <- orthogroups %>% separate_rows(Orthogroup, sep = ",")
+my.files <-  list.files(list.dirs(path = "Desktop/minion", full.names = TRUE, recursive = TRUE), pattern = "quant.sf", full.names = TRUE)
+
+long_data <- orthogroups %>% 
+  gather(key = "sample", value = "value", -Orthogroup)
+
+long_data <- long_data %>% 
+  separate_rows(value, sep = ", ")
+
+long_data$value <- sub("_1$", "", long_data$value)
 
 tpm <- lapply(Sys.glob(my.files), read.table, header = TRUE)
 
@@ -17,7 +25,7 @@ scientific_10 <- function(x) {
   parse(text=gsub("e", " %*% 10^", scales::scientific_format()(x)))
 }
 
-names <- sapply(strsplit(my.files, split="\\/"), function(x)x[7])
+names <- sapply(strsplit(my.files, split="\\/"), function(x)x[4])
 names <- gsub("_quant", "", names)
 names(tpm) <- names
 
@@ -52,15 +60,16 @@ keep_tpm <- subset(keep_tpm, select = -c(Names) )
 
 long <- melt(keep_tpm, id.vars = c("Name"), variable.name = "isolate", value.name = "P")
 long <- merge(x=stats, y=long, by.x="isolate", by.y="variable")[]
-long <- merge(x = long, y = orthogroups_long, by.x = "Name", by.y = "Name", all.x = TRUE)
+long <- merge(x = long, y = long_data, by.x = "Name", by.y = "value", all.x = TRUE)
 long <- long %>% replace_na(list(col1 = "Other", col2 = "Other"))
 
-png("~/Desktop/orthogroup.png", units="in", width=15, height=15, res=300)
-ggboxplot(mouse_comb, x = "type", y = "value",
-          add.params = list(size = 2), color = "stage",
-          add = "jitter") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  facet_wrap("Orthogroup", scales = 'free_x') 
+png("~/Desktop/minion.png", units="in", width=10, height=8, res=300)
+ggbarplot(long, x = "isolate", y = "value",
+          add.params = list(size = 2), color = "Name", fill = "Name") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  facet_wrap("old_stage", scales = 'free_x') +
+  scale_fill_manual(values = coul) +
+  scale_color_manual(values = coul)
 dev.off()
 
 cow_comb <- long[long$host == 'cow', ]
@@ -69,25 +78,52 @@ mouse_comb <- long[long$host == 'mouse', ]
 mouse_comb <- mouse_comb[mouse_comb$type == 'WT', ]
 cow_comb$stage <- as.numeric(cow_comb$stage)
 
-png("~/Desktop/wt_mouse.png", units="in", width=1, height=6, res=300)
-ggbarplot(long, x = "test", y = "value", color = "Name", fill = "Name", legend = "right") +
-  facet_wrap(type + host ~ stage, scales = 'free_x', ncol = 2) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-  scale_fill_manual(values = coul) +
-  scale_color_manual(values = coul)
+
+a <- ggbarplot(long, x = "sample", y = "value", color = "Name", fill = "Name", legend = "right") +
+  facet_wrap(~ cow, scales = 'free_x', ncol = 2) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+
+b <- ggbarplot(long, x = "sample", y = "value", color = "Name", fill = "Name", legend = "right") +
+  facet_wrap(~ pool, scales = 'free_x', ncol = 2) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+
+png("~/Desktop/minion.png", units="in", width=10, height=10, res=300)
+ggarrange(a,b, ncol = 1, nrow = 2,  common.legend = F, legend="right", align = c("hv"), labels = "auto", font.label = list(size = 14, color = "black", face = "bold", family = NULL))
 dev.off()
 
 coul <- brewer.pal(12,"Set3") 
-coul <- colorRampPalette(coul)(-1 + length(unique(cow_comb$Name)))
+coul <- colorRampPalette(coul)(-1 + length(unique(long$Orthogroup)))
 coul <- append(coul, "black")
-cow_comb$stage <- as.numeric(cow_comb$stage)
 
-png("~/Desktop/cow_small.png", units="in", width=20, height=10, res=300)
-ggbarplot(cow_comb, x = "stage", y = "value", color = "Name", fill = "Name", legend = "right") +
-  facet_wrap(~type, scales = 'free_x', ncol = 2) +
+
+long <- long %>%
+  mutate(numeric_value = as.numeric(x_axis))
+
+# Step 2: Arrange the dataframe by the new column in descending order
+long <- long %>%
+  arrange((numeric_value))
+
+
+a <- ggbarplot(long, x = "x_axis", y = "value.x", color = "Orthogroup", fill = "Orthogroup", legend = "right") +
+  facet_wrap(type~old_stage, scales = 'free_x', ncol = 2) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
   scale_fill_manual(values = coul) +
   scale_color_manual(values = coul)
+
+b <- ggbarplot(long, x = "x_axis", y = "value", color = "Orthogroup", fill = "Orthogroup", legend = "right") +
+  facet_wrap(type~old_stage, scales = 'free_x', ncol = 2) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  scale_fill_manual(values = coul) +
+  scale_color_manual(values = coul)
+
+ggarrange(a,b, ncol = 1, nrow = 2,  common.legend = F, legend="right", align = c("hv"), labels = "auto", font.label = list(size = 14, color = "black", face = "bold", family = NULL))
+
+png("~/Desktop/orthogroup_major.png", units="in", width=15, height=15, res=300)
+a
+dev.off()
+
+png("~/Desktop/orthogroup_all.png", units="in", width=15, height=15, res=300)
+b
 dev.off()
 
 png("~/Desktop/pacbio_combined_mouse.png", units="in", width=12, height=12, res=300)
@@ -132,7 +168,7 @@ keep_tpm <- subset(keep_tpm, select = -c(Names) )
 long <- melt(keep_tpm, id.vars = c("Name"), variable.name = "isolate", value.name = "P")
 long <- merge(x=stats, y=long, by.x="isolate", by.y="variable")[]
 
-coul <- brewer.pal(12,"Set3") 
+coul <- brewer.pal(6,"Set3") 
 coul <- colorRampPalette(coul)(-1 + length(unique(long$Name)))
 coul <- append(coul, "black")
 
@@ -211,11 +247,15 @@ a <- ggboxplot(mouse_stats, x = "type", y = "mapping", color = "stage", fill = "
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   ylab("Mapping rate")
 
-b <- ggboxplot(mouse_stats, x = "type", y = "vsgs", color = "stage", fill = "stage", legend = "right") +
+stats <- filter(stats, isolate != "908_d11")
+png("~/Desktop/vsg_summary.png", units="in", width=12, height=10, res=300)
+ggboxplot(stats, x = "x_axis", y = "log(parasites/vsgs)", color = "old_stage", fill = "old_stage", legend = "right") +
+  facet_wrap(type ~ old_stage, scales = 'free_x', ncol = 3) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   ylab("VSG count")
+dev.off()
 
-c <- ggboxplot(mouse_stats, x = "type", y = "log(parasites/vsgs)", color = "stage", fill = "stage", legend = "right") +
+c <- ggboxplot(mouse_stats, x = "type", y = "(parasites/vsgs)", color = "stage", fill = "stage", legend = "right") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   ylab("log(parasite count/ VSG count")
 
